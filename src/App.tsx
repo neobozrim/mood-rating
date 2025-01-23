3cef20a48209f793db7e47321c2d5bd0b2ef502d
Neobozrim <yavor.b.popov@gmail.com>
2025-01-23 20:17:55 +0200

    ts-ignore added to app,tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import React, { useState, useEffect } from 'react';
 import { EventCreation } from './components/EventCreation';
 import { EventDisplay } from './components/EventDisplay';
